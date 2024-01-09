263828627490545b7e6c84011001ba903f4ea9a7
Dahrey <138259597+Dahrey@users.noreply.github.com>
2024-01-09 05:52:54 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
-""" Define square module """
+"""Defines Rectangle module."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Square class body """
+    """class body."""
 
     def __init__(self, size):
-        super().integer_validator("size", size)
-        self.__size = size
-        Rectangle.__init__(self, size, size)
 
-    def area(self):
-        return self.__size * self.__size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
