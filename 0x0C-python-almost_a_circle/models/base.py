8a8ba25590f5a9ccfe0df0dfd050d95865f861d3
Dahrey <138259597+Dahrey@users.noreply.github.com>
2024-01-16 05:58:42 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,11 @@
-#!/usr/bin/python3
-"""Defines Base class"""
 import json
 import csv
 import turtle
 
-
 class Base:
-    """Base class body.
-    """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize Id in a constructor
-        """
         if id is not None:
             self.id = id
         else:
@@ -22,16 +14,12 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ convert result to Json
-        """
         if list_dictionaries is None or list_dictionaries == []:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Save list object to JSON
-        """
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
             if list_objs is None:
@@ -42,16 +30,12 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """Return the deserialization of a JSON string.
-        """
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Return a class instantiated from a dictionary of attributes.
-        """
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new = cls(1, 1)
@@ -62,8 +46,6 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Return a list of classes instantiated from a file of JSON  string.
-        """
         filename = str(cls.__name__) + ".json"
         try:
             with open(filename, "r") as jsonfile:
@@ -74,8 +56,6 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """Write the CSV serialization of a list of objects to a file.
-        """
         filename = cls.__name__ + ".csv"
         with open(filename, "w", newline="") as csvfile:
             if list_objs is None or list_objs == []:
@@ -91,8 +71,6 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
-        """Return a list of class instantiated from a CSV file.
-        """
         filename = cls.__name__ + ".csv"
         try:
             with open(filename, "r", newline="") as csvfile:
@@ -109,8 +87,6 @@ class Base:
 
     @staticmethod
     def draw(cls, list_rectangles, list_squares):
-        """Draw Rectangles and Squares using the turtle module.
-        """
         window = turtle.Screen()
         pen = turtle.Pen()
         figures = list_rectangles + list_squares
