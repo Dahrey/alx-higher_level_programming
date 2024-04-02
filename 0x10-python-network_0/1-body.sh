1cbebe8e90d96e29f14b4383a2e78dd312ebdfbe
Dahrey <138259597+Dahrey@users.noreply.github.com>
2024-04-02 05:20:31 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,2 +1,3 @@
-end a GET request to an URL with curl, and display the body of the response
+#!/bin/bash
+# send a GET request to an URL with curl, and display the body of the response
 curl -sL "$1"
