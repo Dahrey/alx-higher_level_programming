00782d00f2f97294fd2cd6d7e136bf13910b1e98
Dahrey <138259597+Dahrey@users.noreply.github.com>
2024-04-02 05:22:06 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,3 @@
-end a GET request to the URL, and displays the body of the response
+#!/bin/bash
+#Send a GET request to the URL, and displays the body of the response
 curl -sH "X-School-User-Id: 98" "$1"
