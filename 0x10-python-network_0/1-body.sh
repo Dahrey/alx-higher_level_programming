dc2cbc14ba115164fbfb9a4f464bbd88d018b1d3
dahrey <damdre03@gmail.com>
2024-04-01 21:18:51 -0700

    all

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,2 @@
+end a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,2 @@
+splays only the status code of the response.
+curl -s -o /dev/null -w "%{http_code}" "$1"
