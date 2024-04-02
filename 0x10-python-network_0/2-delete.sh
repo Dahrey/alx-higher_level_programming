b7d676d04ab54f97c9d7b5293d674a09b0f9f905
Dahrey <138259597+Dahrey@users.noreply.github.com>
2024-04-02 05:21:27 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,2 +1,3 @@
-t to the URL passed as the first argument and displays the body of the response
+#!/bin/bash
+# DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
